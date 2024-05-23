b5a10fcebfea2082e79ec5b795a308bacb07128d
Leon Windler <leonwindler+github@gmail.com>
2024-05-23 16:32:43 +0200

    d

--- automator.sh
+++ automator.sh
@@ -5,57 +5,33 @@ remote_branches=$(git branch -r)
 
 # Filter the list to only contain 'main' and 'Optimization_X' branches, excluding 'HEAD'.
 # Remove origin/ from the branch names
-filtered_branches=$(echo "$remote_branches" | grep -E "origin/main|origin/Optimization_[0-9]+" | sed 's/origin\///')
-
-# filtered_branches=$(echo "$remote_branches" | grep -E 'origin/(main|Optimization_)' | grep -v 'HEAD')
-
+filtered_branches=$(echo "$remote_branches" | grep -E "origin/main|origin/Optimization_[0-9]+" | grep -v 'HEAD' | sed 's/origin\///')
 
 # Loop through the filtered branches and list all folders in the root directory of the repository
 for branch in $filtered_branches
 do
-
-
     echo
     echo "===================================================================================================="
     # Checkout the branch, pull the latest changes, and echo the name of the branch
-    git checkout $branch
-    git pull origin $branch
+    git checkout "$branch"
+    git pull origin "$branch"
     echo "Current branch: $branch"
 
-
     # Name of the tar file
-    if [ $branch == "main" ]; then
+    if [ "$branch" == "main" ]; then
         tar_file="Baseline"
     else
-        tar_file=$branch
+        tar_file="$branch"
+    fi
 
     # Run the foo script for testing
-    python foo.py $tar_file
+    python foo.py "$tar_file"
 
     # Stage and commit the newly created file "$branch.tar.bz2"
     git add "$tar_file.tar.bz2"
     git commit -m "add $tar_file.tar.bz2, created at $(date)"
-    git push origin $branch
+    git push origin "$branch"
 
     echo "===================================================================================================="
 
-    
-    # current_branch=$branch
-    # # If current branch is 'main', rename it to 'Baseline'
-    # if [ $branch == "main" ]; then
-    #     branch="Baseline"
-    # fi
-
-    # echo "Current branch: $branch"
-
-    # # Pull the latest changes from the remote repository
-    # git pull
-
-    # # Run the foo script for testing
-    # python foo.py $current_branch
-
-    # # Stage and commit the newly created file "$current_branch.tar.bz2"
-    # git add "$current_branch.tar.bz2"
-    # git commit -m "add $current_branch.tar.bz2, created at $(date)"
-    # git push
 done
